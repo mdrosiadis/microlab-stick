41b5bef299443619421c49c64d94244deb195a8a
Mike Drosiadis <mdrosiadis@gmail.com>
2025-02-12 20:00:26 +0200

    dev server fix

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,20 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 import fs from 'fs';
+import { env } from "node:process";
 
 // https://vite.dev/config/
 export default defineConfig({
- server: {
-    https: {
-      key: fs.readFileSync('../localhost.key'),
-      cert: fs.readFileSync('../localhost.crt'),
-    },
-  },
   plugins: [
     svelte(),
     tailwindcss(),
   ],
+  ...(env.NODE_ENV === "development" && {
+   server: {
+      https: {
+        key: fs.readFileSync('../localhost.key'),
+        cert: fs.readFileSync('../localhost.crt'),
+      },
+    },
+  } )
 })
