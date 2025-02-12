2149ebbbb36707f11476572668c1915d3c9a371b
Mike Drosiadis <mdrosiadis@gmail.com>
2025-02-12 20:16:50 +0200

    Set base on GitHub actions

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 import fs from 'fs';
 import { env } from "node:process";
 
+// get correct base name on github actions
+const base = process.env.GITHUB_ACTIONS
+  ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`  
+  : '/';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
     tailwindcss(),
   ],
+  base,
   ...(env.NODE_ENV === "development" && {
    server: {
       https: {
